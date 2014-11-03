a2cd4312c3c5c208feac63813ae28a99073c77fd
albafica <albaficaw@gmail.com>
2014-11-03 13:20:46 +0800

    根据IP地址查询地区信息

--- config.php
+++ config.php
@@ -6,4 +6,10 @@
     'name' => '二维数组多字段排序',
     'description' => '二维数组多字段排序',
   ),
+  1 => 
+  array (
+    'href' => 'iplocation/index.php',
+    'name' => '获取IP地址地区信息',
+    'description' => '根据纯真IP库获取ip地区信息',
+  ),
 );
---
+++ iplocation/index.php
@@ -0,0 +1,29 @@
+<?php
+$location = array();
+if ($_POST['submit']) {
+    include 'IpLocation.class.php';
+    $IpLocation = new IpLocation();
+    $location = $IpLocation->getlocation($_POST['ip']);
+}
+?>
+<!doctype html>
+<html>
+    <head>
+        <title>IP地区查询</title>
+        <meta charset="utf-8" />
+    </head>
+    <body>
+        <form method="post" action="">
+            <label>IP:</label><input type="text" name="ip" value="<?php echo $_POST['ip']; ?>" /><input type="submit" value="查询" name="submit" /><br />
+            <?php if (!empty($location)): ?>
+                <label>IP起始地址:</label>&nbsp;&nbsp;&nbsp;<?php echo $location['beginip']; ?><br />
+                <label>IP结束地址:</label>&nbsp;&nbsp;&nbsp;<?php echo $location['endip']; ?><br />
+                <label>IP所在国家:</label>&nbsp;&nbsp;&nbsp;<?php echo iconv('gb2312', 'utf-8', $location['country']); ?><br />
+                <label>IP所在地区:</label>&nbsp;&nbsp;&nbsp;<?php echo iconv('gb2312', 'utf-8', $location['area']); ?>
+            <?php endif; ?>
+        </form>
+    </body>
+</html>
+
+
+
